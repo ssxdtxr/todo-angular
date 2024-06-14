3ea27a9cc352a35b8fef3986f4ed2170d11d02a6
Egor <120664925+ssxdtxr@users.noreply.github.com>
2024-06-15 00:34:50 +0300

    Настройка проекта + верстка

---
+++ src/app/components/pages/all-tasks/all-tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-tasks.component.html',
+  styles: ''
+})
+export class AllTasksComponent {
+
+}
---
+++ src/app/components/pages/important-tasks/important-tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-important-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './important-tasks.component.html',
+  styles: ''
+})
+export class ImportantTasksComponent {
+
+}
